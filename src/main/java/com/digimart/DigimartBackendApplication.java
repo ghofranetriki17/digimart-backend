d9aafe8e5bb62425c1047f4c5d63c280b1f2c801
agencedigilab <agencedigilab@gmail.com>
2026-02-02 11:23:01 +0100

    Initial commit - Configuration Spring Boot avec PostgreSQL

---
+++ src/main/java/com/digimart/DigimartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigimartBackendApplication.class, args);
+	}
+
+}
